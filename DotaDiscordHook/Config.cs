8be11377b57386a16725ba9b28e82e85c54fb65e
Neeraj Anil <neerajanil@outlook.com>
2019-01-05 14:13:50 -0800

    Modify command functionality to be more usable. Remove functionality that was not being used. (#3)

---
+++ DotaDiscordHook/Config.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotaDiscordHook
+{
+    public static class Config
+    {
+        static string _hookUrl = null;
+        public static string HookUrl
+        {
+            get
+            {
+                if (_hookUrl == null)
+                {
+                    _hookUrl = ConfigurationManager.AppSettings["DiscordWebHookUri"];
+                    Uri hookUri = new Uri(HookUrl);
+                }
+                return _hookUrl;
+            }
+        }
+
+        public static ulong ServerId
+        {
+            get
+            {
+                string serverId = ConfigurationManager.AppSettings["ServerId"];
+                return Convert.ToUInt64(serverId ?? "0");
+            }
+        }
+
+        public static ulong TextChannelId
+        {
+            get
+            {
+                string serverId = ConfigurationManager.AppSettings["TextChannelId"];
+                return Convert.ToUInt64(serverId ?? "0");
+            }
+        }
+
+        public static ulong VoiceChannelId
+        {
+            get
+            {
+                string serverId = ConfigurationManager.AppSettings["VoiceChannelId"];
+                return Convert.ToUInt64(serverId ?? "0");
+            }
+        }
+
+        public static string UserToken
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["UserToken"];
+            }
+        }
+
+        public static string AdminName
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["AdminName"];
+            }
+        }
+
+        public static bool UserConnectMode
+        {
+            get
+            {
+                string flag = ConfigurationManager.AppSettings["UserConnectMode"];
+                return Convert.ToBoolean(flag ?? "false");
+            }
+        }
+
+
+    }
+}
--- DotaDiscordHook/Program.cs
+++ DotaDiscordHook/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
-
+using System.Threading.Tasks;
+using Discord.WebSocket;
 
 namespace DotaDiscordHook
 {
@@ -31,7 +32,7 @@ namespace DotaDiscordHook
             string injectionLibrary = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "DotaChatHook.dll");
             string channelName = null;
             // Create the IPC server using the FileMonitorIPC.ServiceInterface class as a singleton
-            var server = EasyHook.RemoteHooking.IpcCreateServer<DotaChatHook.ServerInterface>(ref channelName, System.Runtime.Remoting.WellKnownObjectMode.Singleton, new DotaChatHook.ServerInterface(Discord.Send));
+            var server = EasyHook.RemoteHooking.IpcCreateServer<DotaChatHook.ServerInterface>(ref channelName, System.Runtime.Remoting.WellKnownObjectMode.Singleton, new DotaChatHook.ServerInterface(DiscordWrapper.Send));
             
             // inject into existing process
             EasyHook.RemoteHooking.Inject(
@@ -41,98 +42,34 @@ namespace DotaDiscordHook
                 channelName         // the parameters to pass into injected library
                                     // ...
             );
+            if (Config.UserConnectMode == true)
+            {
+                DiscordWrapper._client = new DiscordSocketClient();
+                DiscordWrapper._client.Log += LogAsync;
+                DiscordWrapper._client.Ready += ReadyAsync;
+                DiscordWrapper._client.LoginAsync(Discord.TokenType.User, Config.UserToken);
+                DiscordWrapper._client.StartAsync();
+                DiscordWrapper.EnabledUsers.Add(Config.AdminName, new User { Name = Config.AdminName, Admin = true });
+            }
             Console.ReadLine();
             server.StopListening(null);
             Console.ReadLine();
         }
+
+
+        private static Task LogAsync(Discord.LogMessage log)
+        {
+            Console.WriteLine(log.ToString());
+            return Task.FromResult(0);
+        }
+
+         // The Ready event indicates that the client has opened a
+        // connection and it is now safe to access the cache.
+        private static Task ReadyAsync()
+        {
+            Console.WriteLine($"{DiscordWrapper._client.CurrentUser} is connected!");
+
+            return Task.FromResult(0);
+        }
     }
-
-
-    //class Program
-    //{
-    //    // The matching delegate for MessageBeep
-    //    [UnmanagedFunctionPointer(CallingConvention.StdCall, SetLastError = true)]
-    //    delegate bool MessageBeepDelegate(uint uType);
-
-    //    // Import the method so we can call it
-    //    [DllImport("user32.dll")]
-    //    static extern bool MessageBeep(uint uType);
-
-    //    /// <summary>
-    //    /// Our MessageBeep hook handler
-    //    /// </summary>
-    //    static private bool MessageBeepHook(uint uType)
-    //    {
-    //        // We aren't going to call the original at all
-    //        // but we could using: return MessageBeep(uType);
-    //        Console.Write("...intercepted...");
-    //        return false;
-    //    }
-
-    //    /// <summary>
-    //    /// Plays a beep using the native MessageBeep method
-    //    /// </summary>
-    //    static private void PlayMessageBeep()
-    //    {
-    //        Console.Write("    MessageBeep(BeepType.Asterisk) return value: ");
-    //        Console.WriteLine(MessageBeep((uint)BeepType.Asterisk));
-    //    }
-
-    //    static void Main(string[] args)
-    //    {
-    //        Console.WriteLine("Calling MessageBeep with no hook.");
-    //        PlayMessageBeep();
-
-    //        Console.Write("\nPress <enter> to call MessageBeep while hooked by MessageBeepHook:");
-    //        Console.ReadLine();
-
-    //        Console.WriteLine("\nInstalling local hook for user32!MessageBeep");
-    //        // Create the local hook using our MessageBeepDelegate and MessageBeepHook function
-    //        using (var hook = EasyHook.LocalHook.Create(
-    //                EasyHook.LocalHook.GetProcAddress("user32.dll", "MessageBeep"),
-    //                new MessageBeepDelegate(MessageBeepHook),
-    //                null))
-    //        {
-    //            // Only hook this thread (threadId == 0 == GetCurrentThreadId)
-    //            hook.ThreadACL.SetInclusiveACL(new int[] { 0 });
-
-    //            PlayMessageBeep();
-
-    //            Console.Write("\nPress <enter> to disable hook for current thread:");
-    //            Console.ReadLine();
-    //            Console.WriteLine("\nDisabling hook for current thread.");
-    //            // Exclude this thread (threadId == 0 == GetCurrentThreadId)
-    //            hook.ThreadACL.SetExclusiveACL(new int[] { 0 });
-    //            PlayMessageBeep();
-
-    //            Console.Write("\nPress <enter> to uninstall hook and exit.");
-    //            Console.ReadLine();
-    //        } // hook.Dispose() will uninstall the hook for us
-    //    }
-
-    //    public enum BeepType : uint
-    //    {
-    //        /// <summary>
-    //        /// A simple windows beep
-    //        /// </summary>            
-    //        SimpleBeep = 0xFFFFFFFF,
-    //        /// <summary>
-    //        /// A standard windows OK beep
-    //        /// </summary>
-    //        OK = 0x00,
-    //        /// <summary>
-    //        /// A standard windows Question beep
-    //        /// </summary>
-    //        Question = 0x20,
-    //        /// <summary>
-    //        /// A standard windows Exclamation beep
-    //        /// </summary>
-    //        Exclamation = 0x30,
-    //        /// <summary>
-    //        /// A standard windows Asterisk beep
-    //        /// </summary>
-    //        Asterisk = 0x40,
-    //    }
-
-    //}
 }
